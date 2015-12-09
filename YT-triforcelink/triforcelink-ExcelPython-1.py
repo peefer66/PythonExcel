f825932f8f0fce82e02c8b45975add88e42808fc
peefer66 <peefer66@hotmail.com>
2015-12-09 13:00:08 +0000

    Slight mods to py file

--- YT-triforcelink/triforcelink-ExcelPython-1.py
+++ YT-triforcelink/triforcelink-ExcelPython-1.py
@@ -1,17 +1,20 @@
 import xlrd
+try:
+   #  file_location = 'c:\documents and settings\pfieldhouse\my documents\git\pythonexcel\YT-triforcelink\data.xlsx'
+    workbook = xlrd.open_workbook('data.xlsx')
+    sheet = workbook.sheet_by_index(0)
+    """
+    rows and columns are referenced by their index 
+    """
+    print(sheet.cell_value(0 ,0))
+    print(sheet.nrows) #  number of active rows
+    print(sheet.ncols) #  number of active cols 
 
-file_location = 'c:/users/peter/documents/git/pythonexcel/data.xlsx'
-workbook = xlrd.open_workbook(file_location)
-sheet = workbook.sheet_by_index(0)
-"""
-rows and columns are referenced by their index 
-"""
-print(sheet.cell_value(0 ,0))
-print(sheet.nrows) #  number of active rows
-print(sheet.ncols) #  number of active cols 
+    for col in range(sheet.ncols):
+        print(sheet.cell_value(0, col))
 
-for col in range(sheet.ncols):
-    print(sheet.cell_value(0, col))
+except:
+    print('Something went wrong')
 
 
 
